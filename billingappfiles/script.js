4d4a76cda29f50c62a9774b520d262b577ceb63b
rajsquare <68543581+rajsquare@users.noreply.github.com>
2026-07-11 22:19:31 +0530

    Refactor applyModeStyle function
    Remove setting innerText for modeToggle in applyModeStyle function.

--- billingappfiles/script.js
+++ billingappfiles/script.js
@@ -2519,11 +2519,9 @@ updatePricelistBtn.addEventListener(
    MODE + SEARCH UI
 ================================ */
 function applyModeStyle(mode) {
-  modeToggle.innerText = mode;
   modeToggle.style.background =
     mode === "W" ? "#2f3f64" : "#d65353";
 }
-
 modeToggle.addEventListener(
   "click",
   () => {
