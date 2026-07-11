f1fdb1175346e6791a74e8c3bb7a8e235ee2562c
rajsquare <68543581+rajsquare@users.noreply.github.com>
2026-07-11 10:51:08 +0530

    Update initial button state in script.js

--- billingappfiles/pricelistfiles/script.js
+++ billingappfiles/pricelistfiles/script.js
@@ -47,7 +47,6 @@ document.getElementById("billingSwitchBtn").addEventListener("click", () => {
 /* ================================
    INITIAL BUTTON STATE
 ================================ */
-modeToggle.innerText = " ";
 modeToggle.style.background = "#2f3f64";
 
 /* ================================
